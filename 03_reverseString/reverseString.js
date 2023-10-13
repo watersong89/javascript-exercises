9ba525d59d4e15a12a6e74ddf2a05971a444c31c
Watersong89 <watersongordon@gmail.com>
2023-10-13 13:34:21 +0100

    completed further exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let wordArray = word.split("");
+  let wordArrayReversed = wordArray.reverse();
+  let newString = wordArrayReversed.join("");
+  return newString;
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
