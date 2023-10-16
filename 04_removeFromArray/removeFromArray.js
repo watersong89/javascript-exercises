e9a8c80b661da0cb47a6b3c81f129784db9c016a
Watersong89 <watersongordon@gmail.com>
2023-10-16 12:57:47 +0100

    coded further solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+  array = array.filter(arg => !toRemove.includes(arg));
+  return array;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(val1, val2) {
+  let numArray = [];
+  let sum = 0;
+  if (!Number.isInteger(val1) || !Number.isInteger(val2) || val1 < 0 || val2 < 0) {
+    return 'ERROR';
+  }else if (val1 < val2) {
+    for (let i = 0; i < val2; i++) {
+      numArray.push(val1);
+      val1++;
+    }
+  } else if (val1 > val2) {
+    for (let i = 0; i < val1; i++) {
+      numArray.push(val2);
+      val2++;
+    }
+  }
+for (let i = 0; i < numArray.length; i++) {
+  sum += numArray[i];
+}
+return sum;
 };
 
 // Do not edit below this line
