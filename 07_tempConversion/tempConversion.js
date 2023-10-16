639bd6a8d4fe7f654896c8c5f830b68d162586f5
Watersong89 <watersongordon@gmail.com>
2023-10-16 13:43:21 +0100

    created temperature conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let result = (value - 32) * (5 / 9);
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  let result = (value * (9 / 5)) + 32;
+  return Math.round(result * 10) / 10;
 };
+
+
+/* Farenheit to Celsius =
+(f - 32) x (5 / 9)
+
+Celsius to Farenheit =
+(c x (9 / 5)) + 32 */
 
 // Do not edit below this line
 module.exports = {
